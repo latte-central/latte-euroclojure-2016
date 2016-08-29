4ba617b65016405acba1d7a222391fa2c7226163
Frederic Peschanski <not provided>
2016-08-29 09:51:01 +0200

    start of repo

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject latte-euroclojure-2016 "0.0.1-SNAPSHOT"
+  :description "A talk about LaTTe @ Euroclojure 2016."
+  :url "..."
+  :license {:name "MIT Licence"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [latte "0.3.1-SNAPSHOT"]]
+  :codox {:metadata {:doc/format :markdown}
+          :namespaces [latte-euroclojure-2016.core]}
+  :plugins [[lein-codox "0.9.5"]])
+
+
