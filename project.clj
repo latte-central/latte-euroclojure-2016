ff8c393c19609107494279ade100b5d9c168084c
Frederic Peschanski <not provided>
2016-09-04 19:34:22 +0200

    latte version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject latte-euroclojure-2016 "0.0.1-SNAPSHOT"
+(defproject latte-euroclojure-2016 "0.0.2-SNAPSHOT"
   :description "A talk about LaTTe @ Euroclojure 2016."
   :url "..."
   :license {:name "MIT Licence"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [latte "0.3.1-SNAPSHOT"]]
+                 [latte "0.3.2-SNAPSHOT"]]
   :codox {:metadata {:doc/format :markdown}
           :namespaces [latte-euroclojure-2016.core]}
   :plugins [[lein-codox "0.9.5"]])
